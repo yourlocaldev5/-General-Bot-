a9fd06bec5d36698e662a4358ddd078d8703d53c
yourlocaldev5 <yourlocaldev5@gmail.com>
2026-04-16 21:17:13 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#FFFFFF", 
+      secondary: "#8b0000", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
